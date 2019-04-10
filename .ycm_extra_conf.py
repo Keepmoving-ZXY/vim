0296cb918d73c3c2f11ce6847ae1f10f2dcd8927
Xiangyu Zhang <zxy267304@gmail.com>
2019-04-10 16:17:14 +0800

    add tensorflow header location

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -33,11 +33,7 @@ flags = [
 '-I',
 '/usr/include/c++/4.8.5/',
 '-I',
-'/home/zxy/sflagent_c7/branches/sflagent_lenovo_dongde_v0/src/include',
-'-I',
-'/home/zxy/sflagent_c7/branches/sflagent_lenovo_dongde_v0/src/include/lib',
-'-I',
-'/home/zxy/sflagent_c7/branches/sflagent_lenovo_dongde_v0/src/lib/dpdk/src/x86_64-native-linuxapp-gcc/include',
+'/home/zxy/tensorflow/',
 '-I',
 './',
 
@@ -47,13 +43,13 @@ flags = [
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=gnu99',
+'-std=c++11',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
-'c',
+'c++',
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
