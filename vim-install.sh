928fb3fb1760020690f8a311b76f3c4568953691
Cloud User <centos@gpu7.cluster.local>
2020-07-24 11:54:22 +0000

    scripts for centos7

--- vim-install.sh
+++ vim-install.sh
@@ -1,31 +1,29 @@
 cd ~ 
-sudo apt-get update
-sudo apt-get install -y bc curl automake cmake
 
 # install vim dependency
-sudo apt-get install -y libcurl4-openssl-dev 
-sudo apt install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
-	libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-	libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-	python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
+sudo yum install -y ruby ruby-devel lua lua-devel luajit \
+  luajit-devel ctags git python python-devel \
+  python3 python3-devel tcl-devel \
+  perl perl-devel perl-ExtUtils-ParseXS \
+  perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
+  perl-ExtUtils-Embed
 
 git clone https://github.com/vim/vim.git
 cd vim
 
+VIMDIR='/home/zxy/.local/'
 ./configure --with-features=huge \
 	--enable-multibyte \
 	--enable-rubyinterp=yes \
-	--enable-pythoninterp=yes \
-	--with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
 	--enable-python3interp=yes \
 	--with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
 	--enable-perlinterp=yes \
 	--enable-luainterp=yes \
 	--enable-gui=gtk2 \
 	--enable-cscope \
-	--prefix=/home/zxy/.local
+	--prefix=${VIMDIR}
 
-make VIMRUNTIMEDIR=/home/zxy/.local/share/vim/vim81
+make -j 40
 make install
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -61,11 +59,9 @@ vim -c PlugInstall -c q -c q
 
 # compile ycm.core
 cd ~/.vim/autoload/YouCompleteMe
-python install.py --clang-completer
+python3 install.py --clang-completer
 cd ~
 
 rm -rf vim 
 rm -rf ctags
-echo "alias vim=\"/home/zxy/.local/bin/vim\"" >> /home/zxy/.zshrc
-echo "export PATH=/home/zxy/.local/bin:\$PATH" >> /home/zxy/.zshrc
 echo "Finish"
